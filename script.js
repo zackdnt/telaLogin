1eead7e5144abb46fe4385ae5156e2c4d98e36f9
zackdnt <161589741+zackdnt@users.noreply.github.com>
2025-03-19 12:11:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function()[
+
+    const loginInput = document.getElementById("login");
+    const senhaInput = document.getElementById("senha");
+    const enviarBtn = document.getElementById("enviar");
+    
+    enviarBtn.addEventListener('click,' function(event){
+      event.preventDefault()
+    })
+
+])
